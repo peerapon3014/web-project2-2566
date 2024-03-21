1ae8118e9304b2d3ceb861383430d4488881a376
Blackevil7 <phiraphon.l@kkumail.com>
2024-03-21 10:14:54 +0700

    add Function refresh room code when edit

--- src/pages/components/ShowCheckIn.js
+++ src/pages/components/ShowCheckIn.js
@@ -78,7 +78,7 @@ function ShowCheckIN() {
                 if (checkin.id === selectedCheckIn.id) {
                     return {
                         ...checkin, subject: editCheckInSubject,
-                        room_code: editCheckInRoomCode,
+                        room_code: newCheckInRoomCode,
                         room: editCheckInRoom,
                         section: editCheckInSection,
                         class_datetime: editCheckInDateTime
@@ -312,6 +312,33 @@ function ShowCheckIN() {
                                 />
                             </div>
                             <div className="mt-6">
+                                <div className='flex'>
+                                    <Input
+                                        label="รหัสห้อง"
+                                        type="text"
+                                        variant="underlined"
+                                        name="roomcode"
+                                        id="roomcode"
+                                        autoComplete="given-name"
+                                        isReadOnly
+                                        value={newCheckInRoomCode}
+                                        onChange={(e) => setnewCheckInRoomCode(e.target.value)}
+                                        className="max-w-xs"
+                                        endContent={
+                                            <button
+                                                onClick={handleRefreshRoomCode}
+                                                className="focus:outline-none"
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                                </svg>
+                                            </button>
+                                        }
+                                    />
+
+                                </div>
+                            </div>
+                            {/* <div className="mt-6">
                                 <Input
                                     label="รหัสห้อง"
                                     type="text"
@@ -324,7 +351,7 @@ function ShowCheckIN() {
                                     onChange={(e) => seteditCheckInRoomCode(e.target.value)}
                                     className="max-w-xs"
                                 />
-                            </div>
+                            </div> */}
                             <div className="mt-6">
                                 <Input
                                     label="ห้อง"
